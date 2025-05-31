89f100ab48d3d1f05383b156b1a29eb90480f735
Pedro Facanha <pedruluizmf@gmail.com>
2025-05-31 14:12:42 -0700

    Add updatePlan()

--- helpers/recurlyHelpers.ts
+++ helpers/recurlyHelpers.ts
@@ -11,3 +11,41 @@ export async function getPlanId(client: recurly.Client, planCode: string): Promi
 
   throw new Error(`Plan code ${planCode} is invalid or not found.`);
 }
+
+export async function getRecaptcha(token: any, url: string){
+  const verifyResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        secret: process.env.RECAPTCHA_SECRET_KEY as string,
+        response: token
+      })
+    });
+
+    await verifyResponse.json();
+}
+
+export async function updatePlan(customAmount: number, client: recurly.Client, planId: string, purchaseObj: any){
+    // Create an update object
+      const planUpdate = {
+        auto_renew: false,
+        currencies: [
+          {
+            currency: 'CAD',
+            unitAmount: customAmount
+          }
+        ]
+      };      
+      
+      // Update plan info 
+      const updatedPlan = await client.updatePlan(planId, planUpdate)
+      const currentPlanCode = updatedPlan.code;
+      console.log('Updated plan: ', currentPlanCode)
+
+      // Update purchase object
+      purchaseObj.subscriptions[0].planCode = currentPlanCode;
+
+      return purchaseObj;
+}
